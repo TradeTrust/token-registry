81de164b706a880a20d9a5f4622c6aaebe025ddc
Rishabh Singh <rishabh.singh@sgtradextech.com>
2024-11-11 12:34:45 +0530

    fix: lint

--- src/utils/get-event-from-receipt.ts
+++ src/utils/get-event-from-receipt.ts
@@ -1,10 +1,6 @@
-import { TransactionReceipt, ethers } from "ethers";
+import { TransactionReceipt, Interface } from "ethers";
 
-export const getEventFromReceipt = <T extends any>(
-  receipt: TransactionReceipt,
-  topic: string,
-  iface: ethers.Interface
-) => {
+export const getEventFromReceipt = <T extends any>(receipt: TransactionReceipt, topic: string, iface: Interface) => {
   const resLog = receipt.logs.find((log) => iface.parseLog(log)?.name === topic);
   return iface.parseLog(resLog as any) as T;
 };
