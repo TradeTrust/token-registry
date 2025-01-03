e9b12d93e96e3454c23236342c18768872c459b2
Shadab Akhtar <43133098+ShadyAkhtar@users.noreply.github.com>
2025-01-03 12:35:45 +0530

    fix: add support for v6

--- src/utils/get-event-from-receipt.ts
+++ src/utils/get-event-from-receipt.ts
@@ -1,15 +1,32 @@
-import { ContractReceipt, ethers } from "ethers";
 import { TypedEvent } from "@typechain/ethers-v5/static/common";
+import { ContractReceipt, ethers as packedEthers } from "ethers";
+
+const ethers = { ...packedEthers };
+
+if (ethers.version.startsWith("6.")) {
+  (ethers as any).utils = {
+    Interface: (ethers as any).Interface,
+  };
+  (ethers as any).ContractReceipt = (ethers as any).TransactionReceipt;
+}
 
 export const getEventFromReceipt = <T extends TypedEvent<any>>(
   receipt: ContractReceipt,
   topic: string,
+  // @ts-ignore
   iface?: ethers.utils.Interface
 ) => {
-  if (!receipt.events) throw new Error("Events object is undefined");
-  const event = receipt.events.find((evt) => evt.topics[0] === topic);
-  if (!event) throw new Error(`Cannot find topic ${topic}`);
+  if (ethers.version.includes("/5")) {
+    if (!receipt.events) throw new Error("Events object is undefined");
+    const event = receipt.events.find((evt) => evt.topics[0] === topic);
+    if (!event) throw new Error(`Cannot find topic ${topic}`);
 
-  if (iface) return iface.parseLog(event) as unknown as T;
-  return event as T;
+    if (iface) return iface.parseLog(event) as unknown as T;
+    return event as T;
+  }
+  if (ethers.version.startsWith("6")) {
+    const resLog = receipt.logs.find((log: any) => iface.parseLog(log)?.name === topic);
+    return iface.parseLog(resLog as any) as T;
+  }
+  throw new Error("Unsupported ethers version");
 };
