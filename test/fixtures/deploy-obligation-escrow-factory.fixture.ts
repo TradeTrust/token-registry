314db73a76eb2f20790a570a7b737a93eb195de3
manishdex25 <manish.singh@dextech.ai>
2026-07-20 15:56:42 +0530

    refactor: enhance deployObligationEscrowFactoryFixture to support multiple signer types
    - Updated the deployObligationEscrowFactoryFixture function to accept both SignerWithAddress and Signer types for the deployer parameter, improving flexibility in testing scenarios.

--- test/fixtures/deploy-obligation-escrow-factory.fixture.ts
+++ test/fixtures/deploy-obligation-escrow-factory.fixture.ts
@@ -1,8 +1,13 @@
 import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
+import { Signer } from "ethers";
 import { ethers } from "hardhat";
 import { ObligationEscrowFactory } from "@tradetrust/contracts";
 
-export const deployObligationEscrowFactoryFixture = async ({ deployer }: { deployer: SignerWithAddress }) => {
+export const deployObligationEscrowFactoryFixture = async ({
+  deployer,
+}: {
+  deployer: SignerWithAddress | Signer;
+}) => {
   const escrowFactory = await ethers.getContractFactory("ObligationEscrowFactory");
   return (await escrowFactory.connect(deployer).deploy()) as unknown as ObligationEscrowFactory;
 };
